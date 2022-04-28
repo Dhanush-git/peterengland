b1b7cee12cf943cdd144e108810a91db8031469f
Dhanush <suvarnadhanush2000@gmail.com>
2022-04-28 11:05:07 +0530

    removed unused imports

--- src/main/java/com/group2/peterengland/services/offersInterface.java
+++ src/main/java/com/group2/peterengland/services/offersInterface.java
@@ -3,7 +3,6 @@ package com.group2.peterengland.services;
 import com.group2.peterengland.models.Offers;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface offersInterface {
 	
